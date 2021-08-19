fdf4000d3429bd43a556443d4fb8763852c892fa
kumar vikrant <vikezme@gmail.com>
2021-08-20 09:00:12 +0930

    adding file

--- index.js
+++ index.js
@@ -1,48 +1,86 @@
-const inquirer = require('inquirer')
-const fs = require('fs')
+const inquirer = require('inquirer');
+const fs = require('fs');
+const util = require('util');
 
-inquirer.prompt[
+const generatorMarkdown = require('./generateMarkdown')
+
+const questions= [
   {
-    type= "input",
+    type: "input",
     message= "what is the title of the project?",
-    name= "generate readme"
-    validate: (value)=> {if (value) {return true}else {return "i need a value to return"}}
+    name= "Title"
+
   },
   {
-    type= "input",
-    message= "how do you install your app?",
-    name= "installation",
-    validate: (value)=> {if (value) {return true}else {return "i need a value to return"}}
+    type: "input",
+    message= "what is the Description of the project?",
+    name= "Description"
+
   },
   {
-    type= "input",
-    message= "instructions to be followed?",
-    name= "instructions",
-    validate: (value)=> {if (value) {return true}else {return "i need a value to return"}}
+    type: "input",
+    message= "Table of Contents",
+    name= "Table of Contents"
+
   },
   {
-    type= "input",
+    type: "input",
+    message= "What does the user need to install to run this app?",
+    name= "Installation"
+
+  },
+  {
+    type: "input",
     message= "how do you use your app?",
-    name= "usage",
-    validate: (value)=> {if (value) {return true}else {return "i need a value to return"}}
+    name= "Usage"
+
   },
   {
-    type= "list",
+    type: "input"
     message= "what license did you use?",
-    name= "license",
-    validate: (value)=> {if (value) {return true}else {return "i need a value to return"}}
+    name= "License"
+
   },
   {
-    type= "input",
+    type: "input",
+    message= "Contact info for inquiries",
+    name= "Questions"
+
+  },
+  {
+    type: "input",
     message= "github username:",
-    name= "git",
-    validate: (value)=> {if (value) {return true}else {return "i need a value to return"}}
+    name= "Username"
+
   },
   {
-    type= "input",
-    message= "e-mail?",
-    name= "e-mail",
-    validate: (value)=> {if (value) {return true}else {return "i need a value to return"}}
+    type: "input",
+    message= "what is your e-mail?",
+    name= "e-mail"
+
   },
 
 ]
+
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, function (err){
+    console.log(fileName)
+    console.log(data)
+    if(err){
+      return console.log(err)
+    }
+    else{
+      console.log("success");
+    }
+  })
+}
+
+function init(){
+  inquirer.prompt(questions)
+    .then(function(data){
+        writeToFile("README.md", generatorMarkdown(data));
+        console.log(data);
+    }
+  )
+}
+init();
