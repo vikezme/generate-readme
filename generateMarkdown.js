84b03a7c424b4d8caaf94b4c4534c416c91847f6
kumar vikrant <vikezme@gmail.com>
2021-08-20 10:29:45 +0930

    adding file

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,28 +1,30 @@
 
-function generatorMarkdown{
-"# ${data.Title}"
-"http:/github.com/${data.Username}/${data.Title}"
-"# Description"
-"${data.Description}"
-"# Table of Contents"
-"* [Installation](#installation)"
-"* [Usage](#usage)"
-"* [License](#license)"
-"* [Contributing](#contributing)"
-"* [Tests](#tests)"
-"* [Questions](#questions)"
-"# Installation"
-"The following necessary dependencies must be installed to run the application"
-"# Usage"
-"In order to use this app, ${data.Usage}"
-"# License"
-"This project is licensed under the ${data.License} license."
-"![GitHub license](https;:/img.shields.io/badge/license-MIT-blue.svg)"
-"# Contributing"
-"Contributors: ${data.Contributing}"
-"# Tests"
-"The following is needed to run the test: ${data.Tests}"
-"# Questions"
-"If you have any questions about the repo, open an issue or contact ${data.username}"
+function generateMarkdown(data){
+  return `
+# ${data.Title}
+http:/github.com/${data.Username}/${data.Title}
+# Description
+${data.Description}
+# Table of Contents
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [Questions](#questions)
+# Installation
+The following necessary dependencies must be installed to run the application
+# Usage
+In order to use this app, ${data.Usage}
+# License
+This project is licensed under the ${data.License} license.
+![GitHub license](https;:/img.shields.io/badge/license-MIT-blue.svg)
+# Contributing
+Contributors: ${data.Contributing}
+# Tests
+The following is needed to run the test: ${data.Tests}
+# Questions
+If you have any questions about the repo, open an issue or contact ${data.username}
+`
 }
-module.exports = generateMarkdown;
+module.exports = generateMarkdown(data);
--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 const util = require('util');
 
-const generatorMarkdown = require('./generateMarkdown')
+const generatorMarkdown(data) = require('./generateMarkdown')
 
 const questions= [
   {
