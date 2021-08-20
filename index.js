e83b4c0369a36bd47b263184f0a94c92e13dfe64
kumar vikrant <vikezme@gmail.com>
2021-08-20 10:54:44 +0930

    adding file

--- index.js
+++ index.js
@@ -2,61 +2,61 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 const util = require('util');
 
-const generatorMarkdown(data) = require('./generateMarkdown')
+const generatorMarkdown = require('./generateMarkdown')
 
 const questions= [
   {
     type: "input",
-    message= "what is the title of the project?",
-    name= "Title"
+    message: "what is the title of the project?",
+    name: "Title"
 
   },
   {
     type: "input",
-    message= "what is the Description of the project?",
-    name= "Description"
+    message: "what is the Description of the project?",
+    name: "Description"
 
   },
   {
     type: "input",
-    message= "Table of Contents",
-    name= "Table of Contents"
+    message: "Table of Contents",
+    name: "Table of Contents"
 
   },
   {
     type: "input",
-    message= "What does the user need to install to run this app?",
-    name= "Installation"
+    message: "What does the user need to install to run this app?",
+    name: "Installation"
 
   },
   {
     type: "input",
-    message= "how do you use your app?",
-    name= "Usage"
-
-  },
-  {
-    type: "input"
-    message= "what license did you use?",
-    name= "License"
+    message: "how do you use your app?",
+    name: "Usage"
 
   },
   {
     type: "input",
-    message= "Contact info for inquiries",
-    name= "Questions"
+    message: "what license did you use?",
+    name: "License"
 
   },
   {
     type: "input",
-    message= "github username:",
-    name= "Username"
+    message: "Contact info for inquiries",
+    name: "Questions"
 
   },
   {
     type: "input",
-    message= "what is your e-mail?",
-    name= "e-mail"
+    message: "github username:",
+    name: "Username"
+
+  },
+  {
+    type: "input",
+    message: "what is your e-mail?",
+    name: "e-mail"
 
   },
 
